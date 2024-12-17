1539a7b60f8ea4321615041587c848b72ff0608c
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-12-17 18:48:57 +0000

    adding functions

--- code1.py
+++ code1.py
@@ -3,36 +3,47 @@ import numpy as np
 import openpyxl as xl      # To install this library 'pip install openpyxl'
 
 ## Functions Used ##
-def select_sheet_pd(name):
+
+def select_sheet_pd(name):      # Display the first few rows of the sheet from excel workbook
     if name in all_data_pd:
         df = all_data_pd[name]
         print("Data from " + name + ":")
         print(df.head())  # Display the first few rows of the DataFrame
-    
     else:
         print(name + "sheet not found.")
 
 
-## Loading in all workbooks needed ##
+def specific_value_pd(workbook, sheetnm, row, column):
+    data = pd.read_excel(workbook, sheet_name = sheetnm)
+
+    row_index = row - 1
+    column_index = column - 1
+
+    cell_value = df.iloc[row_index, column_index]
+    print(cell_value)
+
+
+def list_sheets_xl(workbook):
+    sheetNames = workbook.sheetnames
+    for name in sheetNames:
+        print(name)
+
+
+## Loading in original workbook ##
 data_xl = xl.load_workbook('The_Data_Landscape_Project_Stats_Macro.xlsx')
-all_data_pd = pd.read_excel('The_Data_Landscape_Project_Stats_Macro.xlsx', sheet_name = None)
 
-
-## Loading workbooks and checking sheets ##
-sheetNames = data_xl.sheetnames
-
+## Checking sheets for original workbook ##
 print('---------------------------------- Original columns -----------------------------------------------')
-for name in sheetNames:
-    print(name)
+list_sheets_xl(data_xl)
 
 ## Deleting Merged data sheet and creating a new one ##
 del data_xl['Merged Data']
 data_xl.create_sheet('merged_data_python')
-sheetNames = data_xl.sheetnames
+
+data_xl.save('The_Data_Landscape_Project_Stats_Macro_updated.xlsx')
 
 print('----------------------------------- New columns ----------------------------------------------------')
-for name in sheetNames:                           ## checking to make sure that new names are implemented
-    print(name)
+list_sheets_xl(data_xl)               ## checking to make sure that new names are implemented
 
 
 ## Copying in team names to new merged sheet ## 
@@ -40,12 +51,14 @@ final_data_sheet = data_xl['merged_data_python']
 
 for i in range(1,34):
     for j in range(1,2):
-
         read_value = data_xl.worksheets[0].cell(row = i, column = j) 
         final_data_sheet.cell(row = i, column = j).value = read_value.value
 
 
+## Save the workbook and reload with pandas ##
+data_xl.save('workbook_for_pandas.xlsx')
+all_data_pd = pd.read_excel('workbook_for_pandas.xlsx', sheet_name = None)
 
-select_sheet_pd(merged_data_python)
+select_sheet_pd('merged_data_python')    ## Checking head of sheet 
 
 print(final_data_sheet.cell(row = 33, column = 1).value)
