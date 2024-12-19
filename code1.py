2842084a9b01251923f7f968e95a9b79accb11bf
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-12-19 19:38:03 +0000

    added headers using for loop with openpyxl

--- code1.py
+++ code1.py
@@ -4,23 +4,31 @@ import openpyxl as xl      # To install this library 'pip install openpyxl'
 
 ## Functions Used ##
 
-def select_sheet_pd(name):      # Display the first few rows of the sheet from excel workbook
-    if name in all_data_pd:
-        df = all_data_pd[name]
+def select_sheet_xl(workbook, name):      # Display the first few rows of the sheet from excel workbook
+    """
+    Display the first few rows of the specified sheet from an openpyxl workbook.
+    
+    Args:
+        workbook: An openpyxl Workbook object.
+        name: The name of the sheet to display.
+    """
+    if name in workbook.sheetnames:
+        df = workbook[name]
         print("Data from " + name + ":")
-        print(df.head())  # Display the first few rows of the DataFrame
+        for row in df.iter_rows(min_row=1, max_row=5, values_only=True):  # Display the first few rows of the DataFrame
+            print(row)    
     else:
         print(name + "sheet not found.")
 
 
-def specific_value_pd(workbook, sheetnm, row, column):
-    data = pd.read_excel(workbook, sheet_name = sheetnm)
+# def specific_value_pd(workbook, sheetnm, row, column):
+#     data = pd.read_excel(workbook, sheet_name = sheetnm)
 
-    row_index = row - 1
-    column_index = column - 1
+#     row_index = row - 1
+#     column_index = column - 1
 
-    cell_value = df.iloc[row_index, column_index]
-    print(cell_value)
+#     cell_value = df.iloc[row_index, column_index]
+#     print(cell_value)
 
 
 def list_sheets_xl(workbook):
@@ -48,28 +56,32 @@ for i in range(1,34):
         read_value = data_xl.worksheets[0].cell(row = i, column = j) 
         final_data_sheet.cell(row = i, column = j).value = read_value.value
 
-
+## Checking sheets of new workbook##
 print('----------------------------------- New columns ----------------------------------------------------')
-list_sheets_xl(data_xl)               ## checking to make sure that new sheet is implemented
+list_sheets_xl(data_xl)               ## checking to make sure that new sheet and 1st column are implemented
+
+## Save the workbook ##
+data_xl.save('transformation_workbook.xlsx')
+
+## Load new workbook for transformations and check head ##
+tf = xl.load_workbook('transformation_workbook.xlsx')
+select_sheet_xl(tf,'merged_data_python')    ## Checking head of sheet 
 
 
-## Save the workbook and reload with pandas ##
-data_xl.save('workbook_for_pandas.xlsx')
+## Getting sheet names to make column headers ##
+tf_shts = tf.sheetnames
+headers = tf_shts[:-1]
+last_sheet_name = tf_shts[-1]
+last_sheet_tf = tf[last_sheet_name]
 
-## Load new workbook with pandas ##
-all_data_pd = pd.read_excel('workbook_for_pandas.xlsx', sheet_name = None)
-select_sheet_pd('merged_data_python')    ## Checking head of sheet 
-
-
-
-
+start_col = 2
+start_row = 1
 
 
+for col_index, header in enumerate(headers,start=start_col):
+    cell = last_sheet_tf.cell(row=start_row, column=col_index)
+    cell.value = header
 
 
+tf.save('transformation_workbook.xlsx')
 
-
-## Adding new column headers to new merged sheet
-sheetNames_pandas = list(all_data_pd.keys())
-headers = sheetNames_pandas[:-1]
-print(headers)
