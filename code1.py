78d03740b47953f5bebd4b85a7086238eead0e77
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-12-19 18:37:42 +0000

    removed extra sheet

--- code1.py
+++ code1.py
@@ -37,14 +37,8 @@ print('---------------------------------- Original columns ---------------------
 list_sheets_xl(data_xl)
 
 ## Deleting Merged data sheet and creating a new one ##
-del data_xl['Merged Data']
 data_xl.create_sheet('merged_data_python')
-
-data_xl.save('The_Data_Landscape_Project_Stats_Macro_Updated.xlsx')
-
-print('----------------------------------- New columns ----------------------------------------------------')
-list_sheets_xl(data_xl)               ## checking to make sure that new sheet is implemented
-
+del data_xl['Merged Data']
 
 ## Copying in team names to new merged sheet ## 
 final_data_sheet = data_xl['merged_data_python']
@@ -55,8 +49,27 @@ for i in range(1,34):
         final_data_sheet.cell(row = i, column = j).value = read_value.value
 
 
+print('----------------------------------- New columns ----------------------------------------------------')
+list_sheets_xl(data_xl)               ## checking to make sure that new sheet is implemented
+
+
 ## Save the workbook and reload with pandas ##
 data_xl.save('workbook_for_pandas.xlsx')
-all_data_pd = pd.read_excel('workbook_for_pandas.xlsx', sheet_name = None)
 
+## Load new workbook with pandas ##
+all_data_pd = pd.read_excel('workbook_for_pandas.xlsx', sheet_name = None)
 select_sheet_pd('merged_data_python')    ## Checking head of sheet 
+
+
+
+
+
+
+
+
+
+
+## Adding new column headers to new merged sheet
+sheetNames_pandas = list(all_data_pd.keys())
+headers = sheetNames_pandas[:-1]
+print(headers)
