95c83a8c69f2e595c9ed4f6f7d73ad7a66025731
abdullahrizmi14 <abdullahrizmi14@gmail.com>
2024-12-25 22:55:29 +0000

    adding unit test and requirment files

--- code1.py
+++ code1.py
@@ -8,7 +8,6 @@ from adjustText import adjust_text
 
 
 ## Functions Used ##
-
 def select_sheet(workbook, name):      # Display the first few rows of the sheet from excel workbook
     """
     Display the first few rows of the specified sheet from an openpyxl workbook.
